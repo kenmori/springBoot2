5ad50a3b2e121836323020686c877f7a179c564a
kenji <kennjimoritata@yahoo.co.jp>
2016-08-06 13:51:15 +0900

    add1

--- hajiboot/src/main/java/com/example/AppConfig.java
+++ hajiboot/src/main/java/com/example/AppConfig.java
@@ -1,14 +1,15 @@
 package com.example;
-
+//Calculatorインターフェイスにインターフェイスに対してどのような実装「Bean 」を管理させるために
 import com.example.app.AddCalculator;
 import com.example.app.Calculator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration  //個のクラスがJavaConfig用のクラスであることを示す
 public class AppConfig {
-	@Bean
-	Calculator calculator(){
+	@Bean//[DIコンテナ]に管理させたい「Bean」を生成するメソッドに「@Bean」アノテーションを付ける
+	Calculator calculator() {
 		return new AddCalculator();
 	}
+	//AddCalculatorインスタンスがCalculator型でcalculatorと言う名前で「DIコンテナ」に「singletone」として管理されるf
 }
